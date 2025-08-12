eef79356e9f711992d97ed5d7475e35335c2c96c
Markus grit:lab <markus.amberla@gmail.com>
2025-08-12 09:54:34 +0300

    q02 to_url

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use doubtful::*;
+use to_url::*;
 
 fn main() {
-    let mut s = "Hello".to_owned();
-
-    println!("Before changing the string: {}", s);
-
-    doubtful(&mut s);
-
-    println!("After changing the string: {}", s);
+    let s = "Hello, world!";
+    println!("'{}' parsed as an URL becomes '{}'", s, to_url(s));
 }
---
+++ to_url/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn to_url(s: &str) -> String {
+    s.chars()
+        .map(|c| {
+            if c == ' ' {
+                "%20".to_owned()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
