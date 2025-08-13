eca80974d9688b29b8ce27c566d388cc9d730e34
Markus grit:lab <markus.amberla@gmail.com>
2025-08-13 09:23:51 +0300

    q03 card deck

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,56 @@
+use rand::{Rng, rng};
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Rank {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Number(u8),
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        Self::translate(rng().random_range(0..=3))
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade][value as usize]
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        Self::translate(rng().random_range(1..14))
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..11 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("incorrect rank")
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    card == &Card{suit: Suit::Spade, rank: Rank::Ace}    
+}
