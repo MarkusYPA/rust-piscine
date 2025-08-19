f82a18cd10f7a6c4e153746500be4987d1fc5b10
Markus grit:lab <markus.amberla@gmail.com>
2025-08-19 13:44:02 +0300

    q05 mobs 6

--- mobs/src/lib.rs
+++ mobs/src/lib.rs
@@ -2,5 +2,10 @@ mod mobs;
 
 pub use mobs::*;
 
+pub use crate::{
+    boss::Boss,
+    member::{Member, Role},
+};
+
 #[cfg(test)]
 mod tests;
--- mobs/src/tests.rs
+++ mobs/src/tests.rs
@@ -1,8 +1,3 @@
-use crate::{
-    boss::Boss,
-    member::{Member, Role},
-};
-
 use super::*;
 
 #[test]
