c723677192fc627d45dfe00e0ac242c779fa758f
Markus grit:lab <markus.amberla@gmail.com>
2025-08-27 15:40:56 +0300

    q08 commits stats

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -1,11 +1,46 @@
+use chrono::IsoWeek;
+use chrono::prelude::*;
+
+#[derive(Debug)]
+struct Week(IsoWeek);
+
+use std::fmt;
+
+impl fmt::Display for Week {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
+
 use std::collections::HashMap;
 
 pub fn commits_per_week(data: &json::JsonValue) -> HashMap<String, u32> {
-    todo!()
+    let mut commits_per_week: HashMap<String, u32> = HashMap::new();
+    for commit in data.members() {
+        let count = commits_per_week
+            .entry(
+                Week(
+                    DateTime::parse_from_rfc3339(&commit["commit"]["author"]["date"].to_string())
+                        .unwrap()
+                        .iso_week(),
+                )
+                .to_string(),
+            )
+            .or_insert(0);
+        *count += 1;
+    }
+    commits_per_week
 }
 
 pub fn commits_per_author(data: &json::JsonValue) -> HashMap<String, u32> {
-    todo!()
+    let mut commits_per_author: HashMap<String, u32> = HashMap::new();
+    for commit in data.members() {
+        let count = commits_per_author
+            .entry(commit["author"]["login"].to_string())
+            .or_insert(0);
+        *count += 1;
+    }
+    commits_per_author
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use commits_stats::{commits_per_week, commits_per_author};
 use std::fs;
 
 fn main() {
-	let contents = fs::read_to_string("commits.json").unwrap();
+	let contents = fs::read_to_string("commits_stats/commits.json").unwrap();
 	let serialized = json::parse(&contents).unwrap();
 	println!("{:?}", commits_per_week(&serialized));
 	println!("{:?}", commits_per_author(&serialized));
