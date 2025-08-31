433c21b4aed21780fdd993cfb79b4b342827df40
Markus grit:lab <markus.amberla@gmail.com>
2025-08-31 20:25:20 +0300

    q08 lifetimes

---
+++ lifetimes/src/tests.rs
@@ -0,0 +1,35 @@
+use super::*;
+use lib::{Kind, TestProperties};
+
+#[test]
+fn fields() {
+    let person = Person {
+        name: "Dijkstra",
+        age: 10,
+    };
+    let test = TestProperties {
+        kind: Kind::Value,
+        name: "age",
+    };
+    test.assert_with_message(&[Box::new(person.clone())], person.age, 10);
+    let test = TestProperties {
+        kind: Kind::Value,
+        name: "name",
+    };
+    test.assert_with_message(&[Box::new(person.clone())], person.name, "Dijkstra");
+}
+
+#[test]
+fn create_person() {
+    let person = Person::new("Leo");
+    let test = TestProperties {
+        kind: Kind::Value,
+        name: "age",
+    };
+    test.assert_with_message(&[Box::new(person.clone())], person.age, 0);
+    let test = TestProperties {
+        kind: Kind::Value,
+        name: "name",
+    };
+    test.assert_with_message(&[Box::new(person.clone())], person.name, "Leo");
+}
