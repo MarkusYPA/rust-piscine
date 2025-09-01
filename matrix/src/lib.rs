9e380e46c2d8c279527826b7a3f2420a53749988
Markus grit:lab <markus.amberla@gmail.com>
2025-09-01 10:27:49 +0300

    no lib to matrix mult

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -3,16 +3,16 @@ use lalgebra_scalar::*;
 #[derive(Debug, PartialEq)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
-impl <T: Scalar<Item = T>> Matrix<T> {
-	pub fn new() -> Matrix<T> {
+impl<T: Scalar<Item = T>> Matrix<T> {
+    pub fn new() -> Matrix<T> {
         Matrix(Vec::new())
-	}
+    }
 
-	pub fn zero(row: usize, col: usize) -> Matrix<T> {
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
         Matrix(vec![vec![T::zero(); col]; row])
-	}
+    }
 
-	pub fn identity(n: usize) -> Matrix<T> {
+    pub fn identity(n: usize) -> Matrix<T> {
         let mut nm = Matrix::new();
 
         for i in 0..n {
@@ -22,7 +22,7 @@ impl <T: Scalar<Item = T>> Matrix<T> {
         }
 
         nm
-	}
+    }
 }
 
 #[cfg(test)]
