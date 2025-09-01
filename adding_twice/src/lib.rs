720fbf82ada352be7ebd4517761e69ad3282f300
Markus grit:lab <markus.amberla@gmail.com>
2025-09-01 12:56:10 +0300

    q10 adding twice with commetns

--- adding_twice/src/lib.rs
+++ adding_twice/src/lib.rs
@@ -5,6 +5,7 @@ pub fn add_curry(v: i32) -> impl Fn(i32) -> i32 {
 // take a function f(x) as parameter, and return a function f(f(x))
 // add two times the value in add_curry to the original value
 pub fn twice<T>(f: impl Fn(T) -> T) -> impl Fn(T) -> T {
+    // f(x) evaluates to a value (i32), not a function. Only f itself is a function/closure.
     move |x| f(f(x))
 }
 
