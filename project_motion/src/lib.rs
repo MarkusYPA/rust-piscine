4840a45f3b04b4769ad55d4df098a3ed743ec176
Markus grit:lab <markus.amberla@gmail.com>
2025-09-01 17:28:54 +0300

    q10 project motion

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -1,3 +1,5 @@
+const G: f32 = -9.8;    // Disgusting not to use 9.81
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Object {
     pub x: f32,
@@ -28,13 +30,40 @@ impl ThrowObject {
 impl Iterator for ThrowObject {
     type Item = Self; // Looks correct
 
-    fn next(&mut self) -> Option<Self::Item> {
-        // advance time by 1
-        // calculate actual position and actual y velocity with new time
-        // None if actual y < 0
-         
-        todo!()
+    // advance time by 1, calculate new position and y velocity
+    fn next(&mut self) -> Option<Self::Item> {        
+        self.time += 1.0;
+
+        // no acceleration component in x velocity
+        let new_pos_x = self.init_position.x + self.init_velocity.x * self.time;
+        // y has acceleration by gravity
+        let new_pos_y = self.init_position.y
+            + self.init_velocity.y * self.time
+            + 0.5 * G * self.time * self.time;
+        // only y velocity changes
+        let new_vel_y = self.init_velocity.y + G * self.time;
+
+        // Round and assign values 
+        self.actual_position = Object {
+            x: round_one(new_pos_x),
+            y: round_one(new_pos_y),
+        };
+        self.actual_velocity = Object {
+            x: round_one(self.init_velocity.x),
+            y: round_one(new_vel_y),
+        };
+
+        // No next when hitting the ground
+        if self.actual_position.y < 0.0 {
+            return None;
+        }
+
+        Some(self.clone())
     }
+}
+
+fn round_one(n: f32) -> f32 {
+    (n*10.0).round() / 10.0
 }
 
 #[cfg(test)]
