8d003d3d9a365c2c5271c3bc565f029a6d51d8c9
Markus grit:lab <markus.amberla@gmail.com>
2025-09-01 16:23:03 +0300

    q10 step iterator

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use step_iterator::*;
 
 fn main() {
-	for v in StepIterator::new(0, 100, 10) {
-		print!("{},", v);
-	}
-	println!();
+    for v in StepIterator::new(0, 100, 10) {
+        print!("{},", v);
+    }
+    println!();
 
-	for v in StepIterator::new(0, 100, 12) {
-		print!("{},", v)
-	}
-	println!();
+    for v in StepIterator::new(0, 100, 12) {
+        print!("{},", v)
+    }
+    println!();
+
+    for (i, v) in StepIterator::new(0.3, 10.0, 0.5).enumerate() {
+        println!("position: {}, value: {}, ", i, v);
+    }
 }
--- step_iterator/src/lib.rs
+++ step_iterator/src/lib.rs
@@ -1,23 +1,23 @@
-//use std::ops::{AddAssign, Sub};
 
+#[allow(dead_code)]
 pub struct StepIterator<T> {
-    beg: T,
+    beg: T,     // not used here, but stores the original starting point
     end: T,
-    started: bool,
+    current: T,
     step: T,
 }
 
 pub use std::ops::Add;
 
 impl<T> StepIterator<T>
-where
-    T: Copy + Add<Output = T> + PartialOrd + PartialEq,
-{
+where 
+    T: Copy,
+ {
     pub fn new(beg: T, end: T, step: T) -> Self {
         Self {
             beg,
             end,
-            started: false,
+            current: beg,
             step,
         }
     }
@@ -25,43 +25,20 @@ where
 
 impl<T> std::iter::Iterator for StepIterator<T>
 where
-    T: PartialEq + Add<Output = T> + PartialOrd + Copy,
+    T: Add<Output = T> + PartialOrd + Copy,
 {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.beg > self.end || self.beg + self.step > self.end {
+        if self.current > self.end {
             return None;
         }
 
-        // first time 'round return 'beginning'
-        if !self.started {
-            self.started = true;
-            return Some(self.beg);
-        }
-
-        // store current value to 'beginning'
-        // won't be able to reset it like this
-        self.beg = self.beg + self.step;
-
-        Some(self.beg)
+        let old_value = self.current;
+        self.current = self.current + self.step;        
+        Some(old_value)
     }
 }
-
-/* impl<T> std::fmt::Display for StepIterator<T>
-where
-    T: std::ops::AddAssign,
-    T: Clone,
-    T: PartialOrd,
-    T: std::ops::Sub,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        //let vals = self.next();
-
-        write!(f, "{}", self.beg)
-        //Ok(())
-    }
-} */
 
 #[cfg(test)]
 mod tests;
